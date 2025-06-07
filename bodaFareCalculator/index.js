12b815b6da33945014f84999f64e96fb1ba185e7
Andy-Ops254 <ghostandy254@outlook.com>
2025-06-07 22:54:38 +0300

    added comments

--- bodaFareCalculator/index.js
+++ bodaFareCalculator/index.js
@@ -1,18 +1,19 @@
+// Declaration and initialization.
 function calculateBodaFare() {
-    let bodaFare= prompt("Unafika wapi Mkubwa? Kilometer ngapi");
-    const distanceInKilometer=parseFloat (bodaFare);
+    let bodaFare= prompt("Unafika wapi Mkubwa? Kilometer ngapi");  //prompt message
+    const distanceInKilometer=parseFloat (bodaFare); //converting input into number using parsefloat.
 
-
+//initial values
     const basefare=50;
     const kilometerCharge=15;
 
-
+//totalfare calculation formular
     let totalFare= basefare + (distanceInKilometer * kilometerCharge);
 
-
+//printing of the values
     console.log (`Uko kwote?: ${distanceInKilometer}km`);
     console.log (`Ukikalia Pikipiki: ${basefare}KES`);
     console.log (`Mpaka Uko ${distanceInKilometer*kilometerCharge}KES`);
     console.log (`Total ${totalFare}KES`);
 }
-calculateBodaFare()
+calculateBodaFare() //invoking/calling the function
